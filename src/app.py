b93c73e967d6fdecec58d84a89df0f4559986879
Francmho <73557750+Francmho@users.noreply.github.com>
2024-07-08 00:06:17 +0000

    Proyecto terminado

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Favorite, Character, Planet, Starship, Comment
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,396 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+##Create an API that connects to a database and implements the following endpoints (very similar to SWAPI.dev or SWAPI.tech):
+#[GET] /people Get a list of all the people in the database.
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    try:
+        # Query all characters from the database
+        characters = Character.query.all()
+        #serialize the rssults
+        characters_list = [character.serialize() for character in characters]
+        #return th serialized list
+        return jsonify(characters_list), 200
+    except Exception as error:
+        return jsonify({"message": "Error fetching people from database"}), 500
 
-    return jsonify(response_body), 200
+
+#[GET] /people/<int:people_id> Get one single person's information.
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_single_person(people_id):
+    try:
+        #do the query for the peopli_id
+        character = Character.query.get(people_id)
+
+        #if character is not found, return 404
+        if character is None:
+            return jsonify({"message": "Person not found"}), 404
+        
+        #serialize the result of request
+        character_data = character.serialize()
+
+        #return serialized person    
+        return jsonify(character_data), 200
+    except Exception as error:  
+        print(error)
+        return jsonify({"message": "Error fetching persona from database"})
+
+
+#[GET] /planets Get a list of all the planets in the database.
+@app.route('/planets', methods=['GET'])
+def get_all_the_planets():
+    try:
+        planets = Planet.query.all()
+
+        planets_lists = [planet.serialize() for planet in planets]
+
+        return jsonify(planets_lists), 200
+    except Exception as  error:
+        print(error)
+        return jsonify({"message": "Error fetching planets from database"}), 500
+
+
+
+#[GET] /planets/<int:planet_id> Get one single planet's information.
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_single_planet(planet_id):
+    try:
+        planet = Planet.query.get(planet_id)
+
+        if planet in None:
+            return jsonify({"message": "planet not found"}), 404
+        
+        planet_data =planet.serialize()
+
+        return jsonify(planet_data), 200
+    except Exception as error:
+        print(error)
+        return jsonify({"message": "Error fetching planet from the dstabase"}), 500
+
+#[GET] /starships Get a list of all the starships in the database.
+@app.route('/starships', methods=['GET'])
+def get_all_the_starships():
+    try:
+        # Realiza la consulta para obtener todas las naves estelares
+        starships = Starship.query.all()
+        
+        # Serializa los resultados de la consulta
+        starships_list = [starship.serialize() for starship in starships]
+
+        # Retorna la lista serializada de naves estelares
+        return jsonify(starships_list), 200
+    except Exception as error:
+        print(error)
+        return jsonify({"message": "Error fetching starships from the database"}), 500
+
+
+#[GET] /starships/<int:planet_id> Get one single planet's information.
+@app.route('/starships/<int:starship>', methods=['GET'])
+def get_a_starship(starship_id):
+    try:
+        starship = Starship.query.get(starship_id)
+
+        if starship in None:
+            return jsonify({"message": "starship not found"}), 404
+        
+        starship_data =starship.serialize()
+
+        return jsonify(starship_data), 200
+    except Exception as error:
+        print(error)
+        return jsonify({"message": "Error fetching starship from the dstabase"})
+
+
+##Additionally, create the following endpoints to allow your StarWars blog to have users and favorites:
+#[POST] /users Create users. 
+@app.route("/user", methods=["POST"])
+def create_user():
+    #Extract data from request
+    data = request.json
+    #Verifying we are receiving all required data in the request
+    email_exists = data.get("email")
+    password_exists = data.get("password")
+    #Returning 400 if data is not correct  
+    if None in [email_exists, password_exists]:
+        return jsonify({
+            "message": "Email and Password are Required"
+        }), 400
+    #Email Verification
+    #We create new user
+    new_user = User(email=email_exists, password=password_exists, is_active=True)
+    try:
+        db.session.add(new_user)
+        db.session.commit()
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"Error in server"}), 500
+    return jsonify({}), 201
+
+
+#[GET] /users Get a list of all the blog post users. [POSTS.COMMENTS]
+@app.route('/users', methods=["GET"])
+def get_users():
+    try:
+        # Realiza la consulta para obtener todas las naves estelares
+        users = User.query.all()
+        
+        # Serializa los resultados de la consulta
+        users_list = [users.serialize() for user in users]
+
+        # Retorna la lista serializada de naves estelares
+        return jsonify(users_list), 200
+    except Exception as error:
+        print(error)
+        return jsonify({"message": "Error fetching starships from the database"}), 500
+
+#[GET] /users/favorites Get all the favorites that belong to the current user.
+@app.route("/users/favorites", methods=["GET"])
+def get_users_favorites(user_id):
+    try:
+        # Obtén el ID del usuario actual (por ejemplo, desde un token de autenticación)
+        user_id = request.args.get('user_id', type=int)  # Suponiendo que el ID del usuario se pasa como parámetro
+
+        user = User.query.get(user_id)
+
+        if user in None:
+            return jsonify({"message": "user has not favorites"}), 404
+        
+        favorites_data =[favorite.serialize() for favorite in user.favorites]
+
+        return jsonify(favorites_data), 200
+    except Exception as error:
+        print(error)
+        return jsonify({"message": "Error fetching starship from the dstabase"})
+
+#[POST] /favorite/planet/<int:planet_id> Add a new favorite planet to the current user with the planet id = planet_id.
+@app.route("/favorites/planet/<int:planet_id>", methods=["POST"])
+def add_favorite_planet_to_user(planet_id):
+    #Extract data from request
+    user_id= request.json.get("user_id")
+    #Verifying we are receiving all required data in the request
+    if not user_id:
+        return jsonify({"message": "User ID is required"}), 400
+    
+    #Check if the planet is already a favorite
+    existing_favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+    if existing_favorite:
+        return jsonify({"message": "Planet already a favorite"}), 400 
+    
+    #Add a new favorite
+    new_favorite = Favorite(user_id= user_id, planet_id= planet_id, is_active=True)
+    try:
+        db.session.add(new_favorite)
+        db.session.commit()
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"Error in server"}), 500
+    return jsonify({"massage": "Planet added to favorites"}), 201
+
+
+#[POST] /favorite/people/<int:people_id> Add new favorite people to the current user with the people id = people_id.
+@app.route("/favorites/people/<int:people_id>", methods=["POST"])
+def add_favorite_character_to_user(people_id):
+    #Extract data from request
+    user_id= request.json.get("user_id")
+    #Verifying we are receiving all required data in the request
+    if not user_id:
+        return jsonify({"message": "User ID is required"}), 400
+    
+    #Check if the people is already a favorite
+    existing_favorite = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first()
+    if existing_favorite:
+        return jsonify({"message": "Character already a favorite"}), 400 
+    
+    #Add a new favorite
+    new_favorite = Favorite(user_id= user_id, people_id= people_id, is_active=True)
+    try:
+        db.session.add(new_favorite)
+        db.session.commit()
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"Error in server"}), 500
+    return jsonify({"massage": "Character added to favorites"}), 201
+
+#[POST] /favorite/starship/<int:starship_id> Add new favorite starship to the current user with the starship id = starship_id.
+@app.route("/favorites/starship/<int:starship_id>", methods=["POST"])
+def add_favorite_starship_to_user(starship_id):
+    #Extract data from request
+    user_id= request.json.get("user_id")
+    #Verifying we are receiving all required data in the request
+    if not user_id:
+        return jsonify({"message": "User ID is required"}), 400
+    
+    #Check if the starship is already a favorite
+    existing_favorite = Favorite.query.filter_by(user_id=user_id, starship_id=starship_id).first()
+    if existing_favorite:
+        return jsonify({"message": "Starship already a favorite"}), 400 
+    
+    #Add a new favorite
+    new_favorite = Favorite(user_id= user_id, starship_id= starship_id, is_active=True)
+    try:
+        db.session.add(new_favorite)
+        db.session.commit()
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"Error in server"}), 500
+    return jsonify({"massage": "Starship added to favorites"}), 201
+
+
+#[DELETE] /favorite/planet/<int:planet_id> Delete a favorite planet with the id = planet_id.
+@app.route("/favorite/planet/<int:planet_id>", methods=["DELETE"])
+def delete_favorite_planet(planet_id):
+    try:
+        # Obtén el ID del usuario actual (por ejemplo, desde un token de autenticación)
+        user_id = request.args.get('user_id', type=int)  # Suponiendo que el ID del usuario se pasa como parámetro
+
+        if not user_id:
+            return jsonify({"message": "User ID is required"}), 400
+        
+        # Busca el registro de favorito correspondiente al usuario y al planeta
+        favorite = Favorite.query.filter_by(user_id=user_id, planet_id=planet_id).first()
+
+        if not favorite:
+            return jsonify({"message": "Favorite planet not found"}), 404
+        
+        # Elimina el registro de favorito
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return jsonify({"message": "Favorite planet deleted successfully"}), 200
+    except Exception as error:
+        print(error)
+        return jsonify({"message": "Error deleting favorite planet from the database"}), 500
+
+#[DELETE] /favorite/people/<int:people_id> Delete a favorite people with the id = people_id.
+@app.route("/favorites/people/<int:people_id>", methods=["DELETE"])
+def delete_favorite_caracter(people_id):
+     try:
+        # Obtén el ID del usuario actual (por ejemplo, desde un token de autenticación)
+        user_id = request.args.get('user_id', type=int)  # Suponiendo que el ID del usuario se pasa como parámetro
+
+        if not user_id:
+            return jsonify({"message": "User ID is required"}), 400
+        
+        # Busca el registro de favorito correspondiente al usuario y al character
+        favorite = Favorite.query.filter_by(user_id=user_id, people_id=people_id).first()
+
+        if not favorite:
+            return jsonify({"message": "Favorite people not found"}), 404
+        
+        # Elimina el registro de favorito
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return jsonify({"message": "Favorite people deleted successfully"}), 200
+     except Exception as error:
+        print(error)
+        return jsonify({"message": "Error deleting favorite planet from the database"}), 500
+
+#[DELETE] /favorite/starship/<int:starship_id> Delete a favorite starship with the id = starship_id.
+@app.route("/favorites/starship/<int:starship_id>", methods=["DELETE"])
+def delete_favorite_starship(starship_id):
+     try:
+        # Obtén el ID del usuario actual (por ejemplo, desde un token de autenticación)
+        user_id = request.args.get('user_id', type=int)  # Suponiendo que el ID del usuario se pasa como parámetro
+
+        if not user_id:
+            return jsonify({"message": "User ID is required"}), 400
+        
+        # Busca el registro de favorito correspondiente al usuario y al character
+        favorite = Favorite.query.filter_by(user_id=user_id, starship_id=starship_id).first()
+
+        if not favorite:
+            return jsonify({"message": "Favorite starship not found"}), 404
+        
+        # Elimina el registro de favorito
+        db.session.delete(favorite)
+        db.session.commit()
+
+        return jsonify({"message": "Favorite starship deleted successfully"}), 200
+     except Exception as error:
+        print(error)
+        return jsonify({"message": "Error deleting favorite planet from the database"}), 500
+    
+
+###Additionally, create the following endpoints to allow users comment elements:
+
+#[POST] /comment/planet/<int:planet_id> Add a new comment planet to the current user with the planet id = planet_id.
+@app.route("/comment/planet/<int:planet_id>", methods=["POST"])
+def add_comment_in_planet(planet_id):
+    #Extract data from request
+    user_id= request.json.get("user_id")
+    comment_text = request.json.get("comment_text")
+
+    #Verifying we are receiving all required data in the request
+    if not user_id or not comment_text:
+        return jsonify({"message": "User ID and comment text are required"}), 400
+    
+    #Add a comment
+    new_comment = Comment(user_id= user_id, planet_id= planet_id, comment_text=comment_text, is_active=True)
+    try:
+        db.session.add(new_comment)
+        db.session.commit()
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"Error in server"}), 500
+    return jsonify({"massage": "Comment added to planet"}), 201
+
+
+#[POST] /comment/people/<int:people_id> Add new comment people to the current user with the people id = people_id.
+@app.route("/comments/people/>int:people_id>", methods=["POST"])
+def add_comment_in_character(people_id):
+    #Extract data from request
+    user_id= request.json.get("user_id")
+    comment_text = request.json.get("comment_text")
+
+    #Verifying we are receiving all required data in the request
+    if not user_id or not comment_text:
+        return jsonify({"message": "User ID and comment text are required"}), 400
+    
+    #Add a comment
+    new_comment = Comment(user_id= user_id, people_id= people_id, comment_text=comment_text, is_active=True)
+    try:
+        db.session.add(new_comment)
+        db.session.commit()
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"Error in server"}), 500
+    return jsonify({"massage": "Comment added to character"}), 201
+
+#[POST] /comment/starship/<int:starship_id> Add new comment starship to the current user with the starship id = starship_id.
+@app.route("/comments/starship/>int:starship_id>", methods=["POST"])
+def add_comment_in_starship(starship_id):
+    #Extract data from request
+    user_id= request.json.get("user_id")
+    comment_text = request.json.get("comment_text")
+
+    #Verifying we are receiving all required data in the request
+    if not user_id or not comment_text:
+        return jsonify({"message": "User ID and comment text are required"}), 400
+    
+    #Add a comment
+    new_comment = Comment(user_id= user_id, starship_id= starship_id, comment_text=comment_text, is_active=True)
+    try:
+        db.session.add(new_comment)
+        db.session.commit()
+    except Exception as error:
+        print(error)
+        db.session.rollback()
+        return jsonify({"message":"Error in server"}), 500
+    return jsonify({"massage": "Comment added to starship"}), 201
+
+###+4 Create also endpoints to add (POST), update (PUT), and delete (DELETE) character, planet and starship. That way all the database information can be managed using the API instead of having to rely on the Flask admin to create the planets and people.
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,14 +2,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+####### USER ######
+
 class User(db.Model):
+    __tablename__ = 'user'
+    # PK
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    #CHILDREN | FAVORITES | ONE TO MANY
+    favorites= db.relationship('Favorite', backref='user', lazy=True)
+    #CHILDREN | COMMENT | ONE TO MANY
+    comment = db.relationship('Comment', backref='user', lazy=True)
+    #CHILDREN | POST | ONE TO MANY
+    # post = db.relationship('post', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -17,3 +27,107 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+
+####### MEDIA DATABASE ######
+
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    # PK
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    age = db.Column(db.Integer)
+    heigth = db.Column(db.Integer)
+    eye_color = db.Column(db.String(50))
+    #CHILDREN | FAVORITE | ONE TO MANY
+    favorite = db.relationship('Favorite', backref='character', lazy=True)
+    #CHILDREN | COMMENTS | ONE TO MANY
+    comment = db.relationship('Comment', backref='character', lazy=True)
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+   # PK
+    id = db.Column(db.Integer, primary_key=True)
+    diameter = db.Column(db.Integer)
+    gravity = db.Column(db.Integer)
+    population = db.Column(db.Integer)
+    climate = db.Column(db.String(250))
+    #CHILDREN | FAVORITE | ONE TO MANY
+    favorites = db.relationship('Favorite', backref='planet', lazy=True)
+    #CHILDREN | COMMENTS | ONE TO MANY
+    comment = db.relationship('Comment', backref='planet', lazy=True)
+
+class Starship(db.Model):
+    __tablename__ = 'starship'
+   # PK 
+    id = db.Column(db.Integer, primary_key=True)
+    model = db.Column(db.String(250))
+    starship_class = db.Column(db.String(250))
+    crew = db.Column(db.Integer)
+    passengers = db.Column(db.Integer)
+    #CHILDREN | FAVORITE | ONE TO MANY
+    favorites = db.relationship('Favorite', backref='starship', lazy=True)
+    #CHILDREN | FAVORITE | ONE TO MANY
+    comments = db.relationship('Comment', backref='starship', lazy=True)
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    # PK
+    id = db.Column(db.Integer, primary_key=True)
+
+    # FK USER.
+    #user = db.relationship('user', backref='favorites', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # FK CHARACTERS.
+    #character = db.relationship('characters', backref='favorites', lazy=True)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    # FK PLANETS.
+    #planet = db.relationship('planet', backref='favorites', lazy=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    # FK STARSHIPS.
+    #starship = db.relationship('starship', backref='favorites', lazy=True)
+    starship_id = db.Column(db.Integer, db.ForeignKey('starship.id'))
+
+
+####### SOCIAL MEDIA STRUCTURE DATABASE ######
+
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+
+    # PK
+    id = db.Column(db.Integer, primary_key=True)
+    # INFO | the comment text
+    comment_text = db.Column(db.String(250))
+    # FK | USER | ONE TO MANY
+    #user = db.relationship('user', back_populates='comments' )
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # FK | CHARACTER | ONE TO MANY
+    #character = db.relationship('character', back_populates='comments' )
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    # FK | PLANET | ONE TO MANY
+    #planet = db.relationship('post', back_populates='comments' )
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    # FK | STARSHIP | ONE TO MANY
+    #starship = db.relationship('post', back_populates='comments' )
+    starship_id = db.Column(db.Integer, db.ForeignKey('starship.id'))
+
+# class Post(db.Model):
+#     __tablename__ = 'post'
+
+#     # PK
+#     id = db.Column(db.Integer, primary_key=True)
+#     # INFO
+#     # FK | USER | ONE TO MANY
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     user = db.relationship('user', back_populates='posts' )
+#     #CHILDREN | COMMENT | ONE TO MANY
+#     comment = db.relationship('comment', back_populates='post' )
+#     #CHILDREN | MEDIA | ONE TO MANY
+#     media = db.relationship('media', back_populates='post' )
+
+
+
+## Draw from SQLAlchemy db.Model
